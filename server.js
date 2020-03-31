88ba3e6a702078c3824cd99e312057cd43865723
Maria Kasińska <mariakasinska17@gmail.com>
2020-03-31 20:59:11 +0200

    init project and use passport

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
 
+const passport = require('passport');
+const session = require('express-session');
+const passportConfig = require('./config/passport');
+
 const app = express();
 
-app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
+app.use(session({ secret: 'anything' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.engine(
+  'hbs',
+  hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' })
+);
 app.set('view engine', '.hbs');
 
 app.use(cors());
@@ -24,6 +35,19 @@ app.get('/user/logged', (req, res) => {
 app.get('/user/no-permission', (req, res) => {
   res.render('noPermission');
 });
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
+  (req, res) => {
+    res.redirect('/user/logged');
+  }
+);
 
 app.use('/', (req, res) => {
   res.status(404).render('notFound');
