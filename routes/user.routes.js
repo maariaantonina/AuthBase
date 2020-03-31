1efd75cc1552228a03ffb5adc910b25a2db0d99b
Maria Kasińska <mariakasinska17@gmail.com>
2020-03-31 21:22:44 +0200

    add profile endpoints

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+  req.user ? res.render('logged') : res.redirect('/no-permission');
 });
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
+});
+
+router.get('/profile', (req, res) => {
+  req.user ? res.render('profile') : res.redirect('/no-permission');
+});
+
+router.get('/profile/settings', (req, res) => {
+  req.user ? res.render('profileSettings') : res.redirect('/no-permission');
 });
 
 module.exports = router;
